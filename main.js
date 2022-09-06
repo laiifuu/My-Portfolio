6728d4d14e145372cd6ce9457dc43e817b4068cf
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-06 09:57:21 +0100

    Fix linter errors

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-const button = document.getElementById("burger-menu");
-const menu = document.querySelector("#nav_list");
+const button = document.getElementById('burger-menu');
+const menu = document.querySelector('#nav_list');
 const menuLinks = [...menu.children];
-const img = button.querySelector("img");
-const body = document.querySelector("body");
+const img = button.querySelector('img');
+const body = document.querySelector('body');
 
-button.addEventListener("click", () => {
-  if (img.getAttribute("toggled") === "false") {
-    img.src = "images/exit-menu.svg";
-    img.setAttribute("toggled", "true");
+button.addEventListener('click', () => {
+  if (img.getAttribute('data-toggled') === 'false') {
+    img.src = 'images/exit-menu.svg';
+    img.setAttribute('data-toggled', 'true');
   } else {
-    img.src = "images/burger-menu.png";
-    img.setAttribute("toggled", "false");
+    img.src = 'images/burger-menu.png';
+    img.setAttribute('data-toggled', 'false');
   }
-  menu.classList.toggle("toggled-menu");
+  menu.classList.toggle('toggled-menu');
   menuLinks.forEach((item) => {
-    item.classList.toggle("nav-link");
+    item.classList.toggle('nav-link');
   });
-  body.classList.toggle("overflow");
+  body.classList.toggle('overflow');
 });
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    menu.classList.remove("toggled-menu");
-    img.setAttribute("src", "images/burger-menu.png");
-    console.log(img);
-    img.setAttribute("toggled", "false");
+  link.addEventListener('click', () => {
+    menu.classList.remove('toggled-menu');
+    img.setAttribute('src', 'images/burger-menu.png');
+    img.setAttribute('data-toggled', 'false');
     menuLinks.forEach((link) => {
-      link.classList.remove("nav-link");
+      link.classList.remove('nav-link');
     });
-    body.classList.remove("overflow");
+    body.classList.remove('overflow');
   });
 });
