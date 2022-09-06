aabf7ec099549ace97f0f41967de24024b9bcf05
adelguit <adeelguitoun@gmail.com>
2022-09-06 09:47:18 +0100

    Finish the menu functionality

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-const button = document.getElementById('burger-menu');
-const menu = document.querySelector('#nav_list');
+const button = document.getElementById("burger-menu");
+const menu = document.querySelector("#nav_list");
+const menuLinks = [...menu.children];
+const img = button.querySelector("img");
+const body = document.querySelector("body");
 
-button.addEventListener('click', () => {
-  const img = button.querySelector('img');
-  if (img.getAttribute('toggled') === 'false') {
-    img.src = 'images/exit-menu.svg';
-    img.setAttribute('toggled', 'true');
+button.addEventListener("click", () => {
+  if (img.getAttribute("toggled") === "false") {
+    img.src = "images/exit-menu.svg";
+    img.setAttribute("toggled", "true");
   } else {
-    img.src = 'images/burger-menu.png';
-    img.setAttribute('toggled', 'false');
+    img.src = "images/burger-menu.png";
+    img.setAttribute("toggled", "false");
   }
-  menu.classList.toggle('toggled-menu');
-  const array = [...menu.children];
-  array.forEach((item) => {
-    item.classList.toggle('nav-link');
+  menu.classList.toggle("toggled-menu");
+  menuLinks.forEach((item) => {
+    item.classList.toggle("nav-link");
+  });
+  body.classList.toggle("overflow");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("toggled-menu");
+    img.setAttribute("src", "images/burger-menu.png");
+    console.log(img);
+    img.setAttribute("toggled", "false");
+    menuLinks.forEach((link) => {
+      link.classList.remove("nav-link");
+    });
+    body.classList.remove("overflow");
   });
 });
