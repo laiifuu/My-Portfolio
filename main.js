dbf010a090c5e1564d6d42c5ec00d5e7f1da357c
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-06 09:06:32 +0100

    Fix linter errors

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-const button = document.getElementById("burger-menu"); 
-const menu = document.querySelector("#nav_list");
+const button = document.getElementById('burger-menu');
+const menu = document.querySelector('#nav_list');
 
-console.log("button: ", button); 
-console.log("menu: ", menu);
-
-for (let item of menu.childNodes){
-    if (item.nodeName.toLowerCase() == "li"){
-        console.log(item);
-    }
-}
-
-button.addEventListener('click', (event) => {
-    const img = button.querySelector("img");
-    console.log(img);
-    console.log(img.getAttribute("toggled"));
-    if (img.getAttribute("toggled") == "false"){
-        img.src = "images/exit-menu.svg";
-        img.setAttribute("toggled", "true");
-    }
-    else {
-        img.src = "images/burger-menu.png";
-        img.setAttribute("toggled", "false");
-
-    }
-    menu.classList.toggle("toggled-menu");
-    for (let item of menu.children){
-        item.classList.toggle("nav-link");
-    }
-
+button.addEventListener('click', () => {
+  const img = button.querySelector('img');
+  if (img.getAttribute('toggled') === 'false') {
+    img.src = 'images/exit-menu.svg';
+    img.setAttribute('toggled', 'true');
+  } else {
+    img.src = 'images/burger-menu.png';
+    img.setAttribute('toggled', 'false');
+  }
+  menu.classList.toggle('toggled-menu');
+  const array = [...menu.children];
+  array.forEach((item) => {
+    item.classList.toggle('nav-link');
+  });
 });
